c0f9d435f3dcdc20ab61a6d803f2410b302fb4f2
Jim <jeremy.marc@gmail.com>
2026-01-27 10:03:53 +0100

    - Fixing ruff config

---
+++ tournament/web/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from tournament.web.views import (
+    TournamentTeamsView,
+    TournamentAccessView,
+    TournamentCreateView,
+    TournamentMatchesView,
+    TournamentSettingsView,
+    TournamentParticipantsView,
+)
+
+app_name = "tournament"
+
+urlpatterns = [
+    path("create/", TournamentCreateView.as_view(), name="create"),
+    path("<int:tournament_id>/", TournamentTeamsView.as_view(), name="teams"),
+    path("<int:tournament_id>/participants/", TournamentParticipantsView.as_view(), name="participants"),
+    path("<int:tournament_id>/matches/", TournamentMatchesView.as_view(), name="matches"),
+    path("<int:tournament_id>/settings/", TournamentSettingsView.as_view(), name="settings"),
+    path("<int:tournament_id>/access/", TournamentAccessView.as_view(), name="access"),
+]
